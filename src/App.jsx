cd97f34c80e839f1aabed2efb5b3e5d731611ebc
Phatthawut <42006909+Phatthawut@users.noreply.github.com>
2024-06-14 13:09:56 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from "react";
 
-import Cart from "./components/cart/Cart.jsx";
+//import Cart from "./components/cart/Cart.jsx";
+import ContactPage from "./pages/ContactPage.jsx";
 
 function App() {
 	const [count, setCount] = useState(0);
 
 	return (
 		<>
-			<Cart />
+			<ContactPage />
 		</>
 	);
 }
---
+++ src/pages/ContactPage.jsx
@@ -0,0 +1,8 @@
+function ContactPage() {
+	return (
+		<div>
+			<h1>Contact page</h1>
+		</div>
+	);
+}
+export default ContactPage;
