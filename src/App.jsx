3c1d88b45d9eee28bff865a66cbc49362bdc4d07
supakarn-t <hikaru8889@gmail.com>
2024-06-14 16:00:03 +0700

    add logo at title, create 1st draft router

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,154 @@
-import { useState } from "react";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import DashboardNav from "./components/DashBoardNav";
 
-import Cart from "./components/cart/Cart.jsx";
+export default function App() {
+	const router = createBrowserRouter([
+		{
+			path: "/",
+			element: (
+				<>
+					{/* <NavBar /> */}
+					<Outlet />
+					{/* <Footer /> */}
+				</>
+			),
+			// errorElement: <ErrorPage/>,
+			children: [
+				{
+					path: "",
+					// element: <HomePage />,
+				},
+				{
+					path: "shop",
+					// element: <ShopPage />,
+				},
+				{
+					path: "cart",
+					// element: <CartPage />,
+				},
+				{
+					path: "cart/checkout",
+					// element: <CheckoutPage />,
+				},
+				{
+					path: "cart/checkout/purchased",
+					// element: <PurchasedPage />,
+				},
+				{
+					path: "contact",
+					// element: <ContactPage />,
+				},
+				{
+					path: "about",
+					// element: <AboutPage />,
+				},
+			],
+		},
 
-function App() {
-	const [count, setCount] = useState(0);
+		{
+			path: "/profile",
+			element: (
+				<>
+					{/* <NavBar /> */}
+					<Outlet />
+					{/* <Footer/> */}
+				</>
+			),
+			// errorElement: <ErrorPage/>,
+			children: [
+				{
+					path: "",
+					// element: <ProfilePage />,
+				},
+				{
+					path: "account",
+					// element: <ProfileAccountPage />,
+				},
+				{
+					path: "payment",
+					// element: <ProfilePaymentPage />,
+				},
+				{
+					path: "order-history",
+					// element: <ProfileHistoryPage />,
+				},
+				{
+					path: "wishlist",
+					// element: <ProfileWishlistPage />,
+				},
+				{
+					path: "create-account",
+					// element: <CreateAccountPage />,
+				},
+				{
+					path: "login",
+					// element: <LoginPage />,
+				},
+				{
+					path: "forget-password",
+					// element: <ForgetPage />,
+				},
+				{
+					path: "reset",
+					// element: <ResetPage />,
+				},
+			],
+		},
 
-	return (
-		<>
-			<Cart />
-		</>
-	);
+		{
+			path: "/dashboard",
+			element: (
+				<>
+					{/* <NavBar /> */}
+					<DashboardNav />
+					<Outlet />
+				</>
+			),
+			// errorElement: <ErrorPage/>,
+			children: [
+				{
+					path: "",
+					// element: <DashboardPage />,
+				},
+				{
+					path: "order",
+					// element: <DashOrderPage />,
+				},
+				{
+					path: "order/view-order",
+					// element: <DashOrderViewPage />,
+				},
+				{
+					path: "customer",
+					// element: <DashCustomerPage />,
+				},
+				{
+					path: "customer/view-customer",
+					// element: <DashCustomerViewPage />,
+				},
+				{
+					path: "product",
+					// element: <DashProductPage />,
+				},
+				{
+					path: "product/add-product",
+					// element: <DashProductAddPage />,
+				},
+				{
+					path: "product/view-product",
+					// element: <DashProductViewPage />,
+				},
+				{
+					path: "admin",
+					// element: <DashAdminPage />,
+				},
+				{
+					path: "admin/admin-setting",
+					// element: <DashAdminSettingPage />,
+				},
+			],
+		},
+	]);
+
+	return <RouterProvider router={router} />;
 }
-
-export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
-import CreateAccountPage from "./components/login/CreateAccountPage.jsx";
-
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-		<CreateAccountPage />
+		<App />
 	</React.StrictMode>
 );
