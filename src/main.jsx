13c6c05615a7f309b8dcb6e0d49467047148ffc1
kanjitta <jittakan2539@gmail.com>
2024-06-15 09:09:06 +0700

    finish desktop-mobile-keep array

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
-import CreateAccountPage from "./components/login/CreateAccountPage.jsx";
+import LoginPage from "./components/login/LoginPage.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-		<CreateAccountPage />
+		<LoginPage />
 	</React.StrictMode>
 );
