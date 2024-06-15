c44fc560dadf509d32d658d4d665644fa329083c
Phatthawut <42006909+Phatthawut@users.noreply.github.com>
2024-06-15 09:33:41 +0700

    reorganize file

--- src/pages/profile/ProfileHistoryPage.jsx
+++
@@ -1,8 +0,0 @@
-function ProfileHistoryPage() {
-	return (
-		<>
-			<h1>Profile History Page</h1>
-		</>
-	);
-}
-export default ProfileHistoryPage;
--- src/pages/profile/ProfileWishlistPage.jsx
+++
@@ -1,8 +0,0 @@
-function ProfileWishlistPage() {
-	return (
-		<>
-			<h1>Profile Page</h1>
-		</>
-	);
-}
-export default ProfileWishlistPage;
