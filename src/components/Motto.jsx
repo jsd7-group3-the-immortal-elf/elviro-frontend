c9d6e6c8a34b16d6f9e189bc25404e1edc133b90
Pongsakon <charlee2543@hotmail.com>
2024-06-16 21:10:12 +0700

    edit Motto tagIcon TagText

--- src/components/Motto.jsx
+++ src/components/Motto.jsx
@@ -4,41 +4,33 @@ import { FaTrophy, FaRegHandshake, FaTruck, FaHeadset } from "react-icons/fa";
 export default function Motto() {
 	return (
 		<div>
-			<section className="max-md:hidden flex justify-center items-center bg-[#B5C18E]/60 h-fit gap-10 px-16 py-6">
-				<div className="flex align-center">
-					<i className="text-4xl my-auto mr-4">
-						<FaTrophy />
-					</i>
+			<section className="max-md:hidden flex justify-center items-center bg-lightgreen h-fit gap-10 px-16 py-6">
+				<div className="flex align-center ">
+					<FaTrophy className="text-5xl my-auto mr-4" />
 					<article>
-						<p className="text-xl font-semibold">High Quality</p>
-						<p>crafted from top materials</p>
+						<h4 className="text-xl font-semibold">High Quality</h4>
+						<h4>crafted from top materials</h4>
 					</article>
 				</div>
 				<div className="flex align-center">
-					<i className="text-4xl my-auto mr-4">
-						<FaRegHandshake />
-					</i>
+					<FaRegHandshake className="text-5xl my-auto mr-4" />
 					<article>
-						<p className="text-xl font-semibold">Warranty Protection</p>
-						<p>Over 2 years</p>
+						<h4 className="text-xl font-semibold">Warranty Protection</h4>
+						<h4>Over 2 years</h4>
 					</article>
 				</div>
 				<div className="flex align-center">
-					<i className="text-4xl my-auto mr-4">
-						<FaTruck />
-					</i>
+					<FaTruck className="text-5xl my-auto mr-4" />
 					<article>
-						<p className="text-xl font-semibold">Free Shipping</p>
-						<p>Order over 1000 Baht</p>
+						<h4 className="text-xl font-semibold">Free Shipping</h4>
+						<h4>Order over 1000 Baht</h4>
 					</article>
 				</div>
 				<div className="flex align-center">
-					<i className="text-4xl my-auto mr-4">
-						<FaHeadset />
-					</i>
+					<FaHeadset className="text-5xl my-auto mr-4" />
 					<article>
-						<p className="text-xl font-semibold">24 / 7 Support</p>
-						<p>Dedicated support</p>
+						<h4 className="text-xl font-semibold">24 / 7 Support</h4>
+						<h4>Dedicated support</h4>
 					</article>
 				</div>
 			</section>
