181dedfd4255cf9c5d08c46107c42efd717f488e
Pongsakon <charlee2543@hotmail.com>
2024-06-18 22:30:47 +0700

    edit Display scale

--- src/components/Motto.jsx
+++ src/components/Motto.jsx
@@ -1,36 +1,36 @@
 // import React from "react";
-import { FaTrophy, FaRegHandshake, FaTruck, FaHeadset } from "react-icons/fa";
+import { FaTrophy, FaRegHandshake, FaTruck, FaHeadset } from "react-icons/fa6";
 
 export default function Motto() {
 	return (
 		<div>
-			<section className="max-md:hidden flex justify-center items-center bg-lightgreen h-fit gap-10 px-16 py-6">
-				<div className="flex align-center ">
+			<section className="hidden lg:flex  justify-center items-center bg-lightgreen gap-10 px-16 py-6">
+				<div className="flex">
 					<FaTrophy className="text-5xl my-auto mr-4" />
 					<article>
-						<h4 className="text-xl font-semibold">High Quality</h4>
-						<h4>crafted from top materials</h4>
+						<h4>High Quality</h4>
+						<p>crafted from top materials</p>
 					</article>
 				</div>
-				<div className="flex align-center">
+				<div className="flex">
 					<FaRegHandshake className="text-5xl my-auto mr-4" />
 					<article>
-						<h4 className="text-xl font-semibold">Warranty Protection</h4>
-						<h4>Over 2 years</h4>
+						<h4>Warranty Protection</h4>
+						<p>Over 2 years</p>
 					</article>
 				</div>
-				<div className="flex align-center">
+				<div className="flex">
 					<FaTruck className="text-5xl my-auto mr-4" />
 					<article>
-						<h4 className="text-xl font-semibold">Free Shipping</h4>
-						<h4>Order over 1000 Baht</h4>
+						<h4>Free Shipping</h4>
+						<p>Order over 1000 Baht</p>
 					</article>
 				</div>
-				<div className="flex align-center">
+				<div className="flex">
 					<FaHeadset className="text-5xl my-auto mr-4" />
 					<article>
-						<h4 className="text-xl font-semibold">24 / 7 Support</h4>
-						<h4>Dedicated support</h4>
+						<h4>24 / 7 Support</h4>
+						<p>Dedicated support</p>
 					</article>
 				</div>
 			</section>
