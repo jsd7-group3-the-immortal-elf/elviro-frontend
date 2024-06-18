76ea6c8e72488d025955e6620fcf1c4269688ea9
kanjitta <jittakan2539@gmail.com>
2024-06-18 21:44:19 +0700

    finish about

--- src/pages/main-web/AboutPage.jsx
+++ src/pages/main-web/AboutPage.jsx
@@ -8,7 +8,7 @@ function AboutPage() {
 			<main>
 				{/* <Banner /> */}
 
-				<section id="content" className="mx-6 my-[66px]">
+				<section id="content" className="flex flex-col mt-16 px-6 items-center">
 					<div className="flex flex-col md:flex-row items-center justify-center gap-5 mb-8">
 						<div className="min-w-[140px] w-[140px] md:w-[160px] ">
 							<img src={Image} alt="Elviro Logo" />
@@ -20,8 +20,11 @@ function AboutPage() {
 							</p>
 						</div>
 					</div>
-					<div id="textContent" className="text-sm md:text-2xl md:mx-20">
-						<p className="mb-5 ">
+					<div
+						id="textContent"
+						className="text-sm md:text-2xl md:mx-20 lg:w-2/3 flex flex-col gap-5"
+					>
+						<p>
 							Furniture is more than just furniture. We understand how
 							meaningful it can be to your growing family. <b>Elviro</b> is
 							derived from an <span className="italic">elf</span>, a magical in
@@ -29,13 +32,13 @@ function AboutPage() {
 							<span className="italic">environment</span>. We are here to make
 							your experience of shopping magical.
 						</p>
-						<p className="mb-5">
+						<p>
 							Not only is our furniture environmentally friendly, and safe for
 							everyone. But we aim to make it suited for all the stages of your
 							family. That’s the motto of Ms. Elvira Yuri, CEO and founder of
 							Elviro.
 						</p>
-						<p className="mb-5">
+						<p>
 							At Elviro, you will find all the furniture for all tastes, which
 							will make shopping enjoyable, and using them even more magical.
 						</p>
