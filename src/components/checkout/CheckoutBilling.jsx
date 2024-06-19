c534c438d801b973287af77c328cf5294dc50158
Pongsakon <charlee2543@hotmail.com>
2024-06-19 23:43:14 +0700

    add HTML CheckoutProduct

--- src/components/checkout/CheckoutBilling.jsx
+++ src/components/checkout/CheckoutBilling.jsx
@@ -6,7 +6,7 @@ export default function CheckoutBilling() {
 			className="flex flex-col items-center w-full max-w-[600px] "
 		>
 			<article id="personal " className=" w-full flex flex-col items-center">
-				<h2 className="my-4">Billing details</h2>
+				<h2 className="mb-4">Billing details</h2>
 				<h3 className="text-darkgreen mb-6">Personal information</h3>
 				{/* --------------------  CheckBox  -------------------- */}
 				{/* <div id="head_personal" className=" mb-6 ">
@@ -24,7 +24,8 @@ export default function CheckoutBilling() {
 					id="details_profile"
 					className="flex flex-col items-center gap-4 w-full"
 				>
-					<label className="px-20 size-full" id="first_name">
+					<label className=" size-full" id="first_name">
+						{/* --------------------  ชื่อ  -------------------- */}
 						<h4>First Name</h4>
 						<input
 							type="text"
@@ -32,7 +33,8 @@ export default function CheckoutBilling() {
 							className="border border-neutral-400 rounded-xl px-4 py-2 mt-1 w-full"
 						/>
 					</label>
-					<label className="px-20 size-full" id="last_name">
+					<label className=" size-full" id="last_name">
+						{/* --------------------  นามสลุก  -------------------- */}
 						<h4>Last Name</h4>
 						<input
 							type="text"
@@ -40,14 +42,16 @@ export default function CheckoutBilling() {
 							className="border border-neutral-400 rounded-xl px-4 py-2 mt-1 w-full"
 						/>
 					</label>
-					<label className="px-20 size-full" id="phone">
+					<label className=" size-full" id="phone">
+						{/* --------------------  เบอร์โทร  -------------------- */}
 						<h4>Phone</h4>
 						<input
 							type="tel"
 							className="phone border border-neutral-400 rounded-xl px-4 py-2 mt-1 w-full"
 						/>
 					</label>
-					<label className="px-20 size-full" id="email">
+					<label className=" size-full" id="email">
+						{/* --------------------  Email  -------------------- */}
 						<h4>Email Address</h4>
 						<input
 							type="email"
@@ -72,35 +76,39 @@ export default function CheckoutBilling() {
 				</div> */}
 				{/* -------------------------ส่วนAddress------------------------- */}
 				<div id="details_contact" className="flex flex-col gap-4 w-full">
-					<label className="px-20 size-full" id="country">
+					<label className=" size-full" id="country">
+						{/* --------------------  ประเทศ  -------------------- */}
 						<h4>Country / Region</h4>
 						<input
 							type="text"
 							className="border border-neutral-400 rounded-xl px-4 py-2 mt-1 w-full"
 						/>
 					</label>
-					<label className="px-20 size-full" id="street_address">
+					<label className=" size-full" id="street_address">
+						{/* --------------------  บ้านเลขที่  -------------------- */}
 						<h4>Street address</h4>
 						<input
 							type="text"
 							className="border border-neutral-400 rounded-xl px-4 py-2 mt-1 w-full"
 						/>
 					</label>
-					<label className="px-20 size-full" id="city">
+					<label className=" size-full" id="city">
+						{/* --------------------  เมือง  -------------------- */}
 						<h4>Town / City</h4>
 						<input
 							type="text"
 							className="border border-neutral-400 rounded-xl px-4 py-2 mt-1 w-full"
 						/>
 					</label>
-					<label className="px-20 size-full" id="province">
+					<label className=" size-full" id="province">
+						{/* --------------------  จังหวะ  -------------------- */}
 						<h4>Province</h4>
 						<input
 							type="text"
 							className="border border-neutral-400 rounded-xl px-4 py-2 mt-1 w-full"
 						/>
 					</label>
-					<label className="px-20 size-full" id="zip_code">
+					<label className=" size-full" id="zip_code">
 						<h4>ZIP code</h4>
 						<input
 							type="text"
