bd9d0430cf0ad9e63fb4b32db6b16fcc7a42bd5c
kanjitta <jittakan2539@gmail.com>
2024-07-09 12:10:25 +0700

    single product--frontend-looks

--- src/components/product/ProductDetail.jsx
+++ src/components/product/ProductDetail.jsx
@@ -14,31 +14,36 @@ function ProductDetail() {
 			</picture>
 
 			{/* ส่วนของ Detail */}
-			<section className="flex flex-col items-start gap-2 mb-5">
+			<section className="flex flex-col items-start gap-2 mb-5 md:gap-5">
 				<h2 className="text-xl font-normal md:text-3xl">Product_Name</h2>
-				<h2 className="text-3xl md:text-6xl">
-					Price <span className="text-xl">Baht</span>
+				<h2 className="text-4xl md:text-6xl">
+					Price <span className="text-xl md:text-2xl">Baht</span>
 				</h2>
-				<h2 className="text-lg font-normal md:text-2xl">Color_Name</h2>
-				<section className="flex gap-2">
+				<h2 className="text-xl font-normal md:text-2xl">Color_Name</h2>
+				<section className="flex gap-2 justify-center">
 					<input
 						type="number"
 						min="0"
 						className="mr-2 p-2 py-3 text-center border border-gray-400 rounded-lg w-1/4"
 						placeholder="1"
 					/>
-					<button className="border border-neutral-500 p-2 px-3 rounded-lg">
+					<button className="w-full border border-neutral-500 p-2 px-3 rounded-lg text-lg hover:bg-brown hover:text-white">
 						Add to Cart
 					</button>
 				</section>
 
 				<hr className="border border-gray-200" />
-				<h2 className="text-lg font-normal">
+				<h2 className="product-detail">
 					Product Id: <span>_id</span>
 				</h2>
-				<h2 className="text-lg font-normal">Tags</h2>
-				<h2 className="text-lg font-normal">
-					Share <span></span>
+				<h2 className="product-detail">
+					Tags <span>_tags</span>
+				</h2>
+				<h2 className="product-detail flex items-center gap-2">
+					Share:
+					<span>
+						<FaShareAlt className="text-2xl" />
+					</span>
 				</h2>
 			</section>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ export default {
 				lightgreen: "#c7d1a6",
 				green: "#b5c18e",
 				darkgreen: "#939d73",
+				brown: "#B99470",
 			},
 		},
 	},
