cc477d1605fcfd4719e0cb0a764247613760ade6
supakarn-t <hikaru8889@gmail.com>
2024-07-10 17:40:26 +0700

    mock page, edit filter layout, add || PORT in axiosInstance

--- src/pages/main-web/ShopPage.jsx
+++ src/pages/main-web/ShopPage.jsx
@@ -3,9 +3,17 @@ import axiosInstance from "../../utils/axiosInstance";
 import Banner from "../../components/Banner";
 import Motto from "../../components/Motto";
 import ProductCard from "../../components/home/ProductCard";
+import { FaSliders, FaXmark } from "react-icons/fa6";
 
 export default function ShopPage() {
 	const [productList, setProductList] = useState([]);
+	const page = 3;
+	const limit = 16;
+	const skip = (page - 1) * limit;
+	const totalProduct = 40;
+	const totalProductEachPage =
+		limit * page > totalProduct ? totalProduct : limit * page;
+	const maxPage = Math.ceil(totalProduct / limit);
 
 	async function getProduct() {
 		try {
@@ -17,6 +25,8 @@ export default function ShopPage() {
 		}
 	}
 
+	function handleFilter() {}
+
 	useEffect(() => {
 		getProduct();
 		window.scrollTo(0, 0);
@@ -27,189 +37,204 @@ export default function ShopPage() {
 			<Banner h1="All Product" h3="All Product" />
 
 			{/* filter section */}
-			<section className="flex justify-between h-16 bg-[#B5C18E]">
-				<ul className="flex items-center px-4 gap-4">
-					<li className="p-4">
-						<button
-							onClick={"showNav('sub-filter'), toggleOverflowHidden('body')"}
-						>
-							<i className="fa-solid fa-sliders"></i>
-						</button>
+			<section className="flex justify-between items-center px-8 h-16 bg-green">
+				<ul className="flex items-center gap-4 h-full">
+					<li onClick={handleFilter} className="flex items-center gap-4 h-full">
+						<FaSliders />
+						<span className="hidden md:flex">Filter</span>
 					</li>
-					<li className="hidden md:flex">Filter</li>
-					<li className="lg:hidden pl-4 py-2 border-l border-neutral-700">
-						Showing 16 results
+					<li className="flex h-full py-2">
+						<div className="border-l border-neutral-700 "></div>
 					</li>
-					<li className="hidden lg:flex pl-4 py-2 border-l border-neutral-700">
-						Showing 1-16 of 32 results
+
+					<li className="flex items-center lg:hidden h-full">
+						Showing {totalProduct} results
+					</li>
+					<li className="items-center hidden lg:flex h-full">
+						Showing {skip + 1}-{totalProductEachPage} of {totalProduct} results
 					</li>
 				</ul>
 
-				<ul className="hidden lg:flex items-center px-4 gap-4">
-					<li className="">Show</li>
-					<li className="">
+				<ul className="hidden lg:flex items-center gap-4 h-full">
+					<li>Show</li>
+					<li>
 						<input
 							type="number"
-							value="16"
+							name="limit"
+							value={limit}
 							className="bg-white rounded w-10 py-1 pl-2"
 						/>
 					</li>
 					<li>Sort by</li>
 					<li>
-						<input
-							type="text"
-							value="Default"
-							className="bg-white rounded w-32 py-1 pl-2"
-						/>
+						<select name="sort" className="bg-white rounded w-32 py-1 pl-2">
+							<option value="Default">Default</option>
+							<option value="Price">Price</option>
+							<option value="Name">Name</option>
+							<option value="Default">Default</option>
+						</select>
 					</li>
 				</ul>
 			</section>
 
 			<section
 				id="sub-filter"
-				className="hidden fixed top-0 left-0 z-50 bg-black/50 w-full h-full"
+				className="fixed top-0 left-0 z-50 bg-black/50 w-full h-full"
 			>
-				<div className="relative w-full h-full md:w-96 mt-20 md:mt-0 flex flex-col bg-white rounded-t-xl md:rounded-s-none md:rounded-r-xl">
-					<div className="py-6 text-center border-b relative">
+				<div className="relative w-full h-full md:w-96 mt-20 md:mt-0 flex flex-col bg-white rounded-t-xl md:rounded-s-none md:rounded-r-xl overflow-hidden">
+					<div className="py-6 text-center border-b relative bg-lightgreen">
 						<span>Filter and Sort</span>
-						<button
-							onClick="showNav('sub-filter'), toggleOverflowHidden('body')"
-							className="absolute right-6"
-						>
-							<i className="fa-solid fa-xmark"></i>
-						</button>
+						<FaXmark className="text-xl absolute top-6 right-6" />
 					</div>
 					<ul className="w-full bg-white overflow-y-scroll">
 						<li className="border-b">
-							<button onClick="showNav('sub-sort-by')" className="w-full py-6">
+							<button onClick="showNav('sub-sort-by')" className="w-full h-16">
 								Sort by
 							</button>
+							<div className="flex flex-col bg-neutral-100">
+								<label className="flex items-center gap-4 ml-8 h-16 accent-lime-600 cursor-pointer">
+									<input type="radio" name="rooms" className="w-4 h-4" />
+									Price: low to high
+								</label>
+								<hr />
+								<label className="flex items-center gap-4 ml-8 h-16 accent-lime-600 cursor-pointer">
+									<input type="radio" name="rooms" className="w-4 h-4" />
+									Price: high to low
+								</label>
+								<hr />
+								<label className="flex items-center gap-4 ml-8 h-16 accent-lime-600 cursor-pointer">
+									<input type="radio" name="rooms" className="w-4 h-4" />
+									Name
+								</label>
+								<hr />
+								<label className="flex items-center gap-4 ml-8 h-16 accent-lime-600 cursor-pointer">
+									<input type="radio" name="rooms" className="w-4 h-4" />
+									Newest
+								</label>
+							</div>
 						</li>
-						<ul id="sub-sort-by" className="hidden bg-neutral-100">
-							<li className="border-b">
-								<button className="w-full py-6">must be input radio</button>
-							</li>
-							<li className="border-b">
-								<button className="w-full py-6">Price: low to high</button>
-							</li>
-							<li className="border-b">
-								<button className="w-full py-6">Price: high to low</button>
-							</li>
-						</ul>
 
 						<li className="border-b">
-							<button onClick="showNav('sub-category')" className="w-full py-6">
+							<button onClick="showNav('sub-category')" className="w-full h-16">
+								Room
+							</button>
+							<hr />
+							<div className="flex flex-col bg-neutral-100">
+								<label className="flex items-center gap-4 ml-8 h-16 accent-lime-600 cursor-pointer">
+									<input
+										type="checkbox"
+										name="rooms"
+										value="Bedroom"
+										className="w-4 h-4"
+									/>
+									Bedroom
+								</label>
+								<hr />
+								<label className="flex items-center gap-4 ml-8 h-16 accent-lime-600 cursor-pointer">
+									<input
+										type="checkbox"
+										name="rooms"
+										value="Living Room"
+										className="w-4 h-4"
+									/>
+									Living Room
+								</label>
+								<hr />
+								<label className="flex items-center gap-4 ml-8 h-16 accent-lime-600 cursor-pointer">
+									<input
+										type="checkbox"
+										name="rooms"
+										value="Kitchen"
+										className="w-4 h-4"
+									/>
+									Kitchen
+								</label>
+							</div>
+						</li>
+
+						<li className="border-b">
+							<button onClick="showNav('sub-category')" className="w-full h-16">
 								Category
 							</button>
+							<div className="flex flex-col bg-neutral-100">
+								<label className="flex items-center gap-4 ml-8 h-16 accent-lime-600 cursor-pointer">
+									<input type="checkbox" name="rooms" className="w-4 h-4" />
+									Bed
+								</label>
+								<hr />
+								<label className="flex items-center gap-4 ml-8 h-16 accent-lime-600 cursor-pointer">
+									<input type="checkbox" name="rooms" className="w-4 h-4" />
+									Seating
+								</label>
+								<hr />
+								<label className="flex items-center gap-4 ml-8 h-16 accent-lime-600 cursor-pointer">
+									<input type="checkbox" name="rooms" className="w-4 h-4" />
+									Table
+								</label>
+								<hr />
+								<label className="flex items-center gap-4 ml-8 h-16 accent-lime-600 cursor-pointer">
+									<input type="checkbox" name="rooms" className="w-4 h-4" />
+									Storage
+								</label>
+							</div>
 						</li>
-						<ul id="sub-category" className="hidden bg-neutral-100">
-							<li className="border-b">
-								<button className="w-full py-6">this is tag</button>
-							</li>
-							<li className="border-b">
-								<button className="w-full py-6">Living Room</button>
-							</li>
-							<li className="border-b">
-								<button className="w-full py-6">Kitchen</button>
-							</li>
-						</ul>
-
-						<li className="border-b">
-							<button onClick="showNav('sub-color')" className="w-full py-6">
-								Color
-							</button>
-						</li>
-						<ul id="sub-color" className="hidden bg-neutral-100">
-							<li className="border-b">
-								<button className="w-full py-6">Cream</button>
-							</li>
-							<li className="border-b">
-								<button className="w-full py-6">Red</button>
-							</li>
-							<li className="border-b">
-								<button className="w-full py-6">Blue</button>
-							</li>
-						</ul>
-
-						<li className="border-b">
-							<button
-								onClick="showNav('sub-size')"
-								className="block w-full py-6"
-							>
-								Size
-							</button>
-						</li>
-						<ul id="sub-size" className="hidden bg-neutral-100">
-							<li className="border-b">
-								<button className="w-full py-6">maybe input form</button>
-							</li>
-							<li className="border-b">
-								<button className="w-full py-6">min width - max width</button>
-							</li>
-							<li className="border-b">
-								<button className="w-full py-6">min height - max height</button>
-							</li>
-						</ul>
 
 						<li className="border-b">
 							<button
 								onClick="showNav('sub-price')"
-								className="block w-full py-6"
+								className="block w-full h-16"
 							>
 								Price
 							</button>
+							<div className="flex flex-col bg-neutral-100">
+								<div className="flex justify-between">
+									<p>min</p> <p>max</p>
+								</div>
+								<input type="range" name="" id="" />
+							</div>
 						</li>
-						<ul id="sub-price" className="hidden bg-neutral-100">
-							<li className="border-b">
-								<button className="w-full py-6">this is checkbox</button>
-							</li>
-							<li className="border-b">
-								<button className="w-full py-6">THB 0 - 199</button>
-							</li>
-							<li className="border-b">
-								<button className="w-full py-6">THB 200 - 399</button>
-							</li>
-						</ul>
 
 						<li className="border-b">
 							<button
-								onClick="showNav('sub-rating')"
-								className="block w-full py-6"
+								onClick="showNav('sub-size')"
+								className="block w-full h-16"
 							>
-								Rating
+								Dimension
 							</button>
+							<div className="flex flex-col bg-neutral-100">
+								<label className="flex flex-col gap-4 ml-8 h-32 accent-lime-600 cursor-pointer">
+									Width
+									<div className="flex justify-between">
+										<p>min</p> <p>max</p>
+									</div>
+									<input type="range" name="" id="" />
+								</label>
+								<hr />
+								<label className="flex flex-col gap-4 ml-8 h-32 accent-lime-600 cursor-pointer">
+									Depth
+									<div className="flex justify-between">
+										<p>min</p> <p>max</p>
+									</div>
+									<input type="range" name="" id="" />
+								</label>
+								<hr />
+								<label className="flex flex-col gap-4 ml-8 h-32 accent-lime-600 cursor-pointer">
+									Depth (Extent)
+									<div className="flex justify-between">
+										<p>min</p> <p>max</p>
+									</div>
+									<input type="range" name="" id="" />
+								</label>
+								<hr />
+								<label className="flex flex-col gap-4 ml-8 h-32 accent-lime-600 cursor-pointer">
+									Height
+									<div className="flex justify-between">
+										<p>min</p> <p>max</p>
+									</div>
+									<input type="range" name="" id="" />
+								</label>
+							</div>
 						</li>
-						<ul id="sub-rating" className="hidden bg-neutral-100">
-							<li className="border-b">
-								<button className="w-full py-6">checkbox</button>
-							</li>
-							<li className="border-b">
-								<button className="w-full py-6">5</button>
-							</li>
-							<li className="border-b">
-								<button className="w-full py-6">4</button>
-							</li>
-						</ul>
-
-						<li className="border-b">
-							<button
-								onClick="showNav('sub-some')"
-								className="block w-full py-6"
-							>
-								Something
-							</button>
-						</li>
-						<ul id="sub-some" className="hidden bg-neutral-100">
-							<li className="border-b">
-								<button className="w-full py-6">checkbox</button>
-							</li>
-							<li className="border-b">
-								<button className="w-full py-6">5</button>
-							</li>
-							<li className="border-b">
-								<button className="w-full py-6">4</button>
-							</li>
-						</ul>
 					</ul>
 					<div className="flex gap-4 p-6 text-center w-full bg-white">
 						<button
@@ -230,7 +255,7 @@ export default function ShopPage() {
 
 			{/* product section */}
 			<section className="flex flex-col items-center gap-6 p-4 md:p-12">
-				<h2>Our Product</h2>
+				<h2>Our Product </h2>
 				<div className="grid grid-cols-2 lg:grid-cols-4 pt-4 gap-4 md:gap-8">
 					{productList.map((product) => (
 						<ProductCard key={product._id} product={product} />
@@ -248,7 +273,7 @@ export default function ShopPage() {
 						<a href="">3</a>
 					</li>
 					<li className="px-3 py-2 bg-[#B5C18E]/40 rounded">
-						<a href="">Next</a>
+						<a href="">{maxPage}</a>
 					</li>
 				</ul>
 			</section>
--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const axiosInstance = axios.create({
-	baseURL: import.meta.env.VITE_BACKEND_URL,
+	baseURL: import.meta.env.VITE_BACKEND_URL || import.meta.env.VITE_PORT,
 	timeout: 10000,
 	headers: {
 		"Content-Type": "application/json",
