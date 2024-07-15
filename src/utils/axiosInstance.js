51339ebb38cac41890a33750c0adb3c350db84f5
supakarn-t <hikaru8889@gmail.com>
2024-07-16 01:24:50 +0700

    install react-cookie, setup token from cookie

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -4,14 +4,17 @@ import Cookies from "js-cookie";
 const axiosInstance = axios.create({
 	baseURL: import.meta.env.VITE_BACKEND_URL || import.meta.env.VITE_PORT,
 	timeout: 10000,
+	withCredentials: true,
 	headers: {
 		"Content-Type": "application/json",
 	},
 });
+// axios.defaults.withCredentials = true;
 
 axiosInstance.interceptors.request.use(
 	(config) => {
-		const token = Cookies.get("token");
+		const token = Cookies.get("access_token");
+		// console.log("axiosInstance", token);
 		if (token) {
 			config.headers.Authorization = `Bearer ${token}`;
 		}
