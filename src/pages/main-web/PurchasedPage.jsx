0a5afd0cfe106965b83a403ab0ce968c80cf1934
Pongsakon <charlee2543@hotmail.com>
2024-07-16 16:23:55 +0700

    Edit CSS

--- src/pages/main-web/PurchasedPage.jsx
+++ src/pages/main-web/PurchasedPage.jsx
@@ -1,24 +1,20 @@
 import { FaCircleCheck } from "react-icons/fa6";
-import Motto from "../../components/Motto";
 import { Link } from "react-router-dom";
 
 export default function PurchasedPage() {
 	return (
-		<>
-			<section className="flex flex-col justify-between items-center w-full">
-				<div className="h-full w-auto my-48 flex flex-col justify-center items-center gap-4">
-					<FaCircleCheck className=" text-9xl text-green mb-8" />
-					<h1>Payment Successful</h1>
-					<h3>Thank you for your purchased</h3>
-					<Link
-						to={"/"}
-						className="text-center mt-8 p-2 w-full text-3xl rounded-full text-black bg-lightgreen hover:text-white border border-neutral-50  hover:bg-brown hover:shadow-2xl  "
-					>
-						Continue
-					</Link>
-				</div>
-				<Motto />
-			</section>
-		</>
+		<section className="flex flex-col justify-center items-center w-full h-[calc(100vh-64px)] gap-8 ">
+			{/* <div className="h-full w-auto my-48 flex flex-col justify-center items-center gap-4"> */}
+			<FaCircleCheck className=" text-9xl text-green mb-8" />
+			<h2>Payment Successful</h2>
+			<h4>Thank you for your purchased</h4>
+			<Link
+				to={"/"}
+				className="text-center mt-8 btn w-2/3 md:w-1/2 xl:w-1/4 text-3xl rounded-full bg-lightgreen hover:text-white  hover:bg-darkgreen hover:shadow-2xl  "
+			>
+				Continue
+			</Link>
+			{/* </div> */}
+		</section>
 	);
 }
