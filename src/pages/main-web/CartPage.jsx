a8af4d0341ab73a48c20a061e15e4f7cd5bd585c
supakarn-t <hikaru8889@gmail.com>
2024-07-17 11:02:33 +0700

    cal total price at cart

--- src/pages/main-web/CartPage.jsx
+++ src/pages/main-web/CartPage.jsx
@@ -5,8 +5,7 @@ import CartBanner from "../../components/cart/CartBanner";
 import CartList from "../../components/cart/CartList";
 import CartTotal from "../../components/cart/CartTotal";
 
-function CartPage({tokenUserId}) {
-
+function CartPage({ tokenUserId }) {
 	// const { userId } = useParams();
 	const [totalPrice, setTotalPrice] = useState(0);
 
@@ -19,7 +18,11 @@ function CartPage({tokenUserId}) {
 			<CartBanner />
 			<div className="flex flex-col md:flex-row justify-center gap-4">
 				<div className="w-full md:basis-2/3">
-					<CartList tokenUserId = {tokenUserId} setTotalPrice={setTotalPrice} />
+					<CartList
+						tokenUserId={tokenUserId}
+						totalPrice={totalPrice}
+						setTotalPrice={setTotalPrice}
+					/>
 				</div>
 				<div className="w-full md:basis-1/3">
 					<CartTotal totalPrice={totalPrice} />
@@ -34,38 +37,34 @@ function CartPage({tokenUserId}) {
 	);
 }
 
-
-
-
-	CartPage.propTypes = {
-    tokenUserId: PropTypes.string,
+CartPage.propTypes = {
+	tokenUserId: PropTypes.string,
 };
 
 export default CartPage;
 
-	// const priceList = [
-	// 	{
-	// 		productPicture: "/images/mockup-sofa.png",
-	// 		productName: "PÄRUP sofa",
-	// 		productQuanlity: "3",
-	// 		productPrice: "8999",
-	// 	},
-	// 	{
-	// 		productPicture: "/images/mockup-sofa.png",
-	// 		productName: "VIMLE sofa",
-	// 		productQuanlity: "1",
-	// 		productPrice: "15999",
-	// 	},
-	// 	{
-	// 		productPicture: "/images/mockup-sofa.png",
-	// 		productName: "GLOSTAD sofa",
-	// 		productQuanlity: "2",
-	// 		productPrice: "2999",
-	// 	},
-	// ];
+// const priceList = [
+// 	{
+// 		productPicture: "/images/mockup-sofa.png",
+// 		productName: "PÄRUP sofa",
+// 		productQuanlity: "3",
+// 		productPrice: "8999",
+// 	},
+// 	{
+// 		productPicture: "/images/mockup-sofa.png",
+// 		productName: "VIMLE sofa",
+// 		productQuanlity: "1",
+// 		productPrice: "15999",
+// 	},
+// 	{
+// 		productPicture: "/images/mockup-sofa.png",
+// 		productName: "GLOSTAD sofa",
+// 		productQuanlity: "2",
+// 		productPrice: "2999",
+// 	},
+// ];
 
-	
-	// const totalPrice = priceList.reduce((acc, product) => {
-	// 	const subtotal = product.productQuanlity * product.productPrice;
-	// 	return acc + subtotal;
-	// }, 0);
+// const totalPrice = priceList.reduce((acc, product) => {
+// 	const subtotal = product.productQuanlity * product.productPrice;
+// 	return acc + subtotal;
+// }, 0);
